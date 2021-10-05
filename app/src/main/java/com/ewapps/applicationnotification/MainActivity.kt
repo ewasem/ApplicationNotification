41e966df1994d51403f721ba60ae3c39468bb95e
ewasem <eduardowasem@outlook.com>
2021-10-05 15:25:26 -0300

    Local notification implementado

--- app/src/main/java/com/ewapps/applicationnotification/MainActivity.kt
+++ app/src/main/java/com/ewapps/applicationnotification/MainActivity.kt
@@ -2,10 +2,19 @@ package com.ewapps.applicationnotification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnSend: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnSend = findViewById(R.id.btn_send_notification)
+
+        btnSend.setOnClickListener {
+            this.showNotification("12340", "Bootcamp Kotlin", "Kotlin curso android")
+        }
     }
 }
